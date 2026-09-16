06c4f831069a0928574b533b4b04a7296fddded2
zoyasyd <zsyed722@gmail.com>
2026-09-16 01:42:14 +0000

    added changes to font and javascript stuff to the html file for the IKEA logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 // main.js
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2"); 
+
+targetElement.addEventListener("mouseover",function() {
+    this.innerText = "TIME";
+    this.style = "cursor: wait"; 
+}); 
+
+targetElement.addEventListener("mouseout",function() {
+    this.innerText = "IKEA"; 
+    this.style = "cursor: default"; 
+}); 
